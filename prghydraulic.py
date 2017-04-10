177c96b1662cf98b4b849d24122ad81baf0292d9
noisywiz <noisywizard@gmail.com>
2017-04-10 21:19:58 +0300

    Update prghydraulic.py

--- prghydraulic.py
+++ prghydraulic.py
@@ -3,6 +3,111 @@
 """Class for hydraulic calculation."""
 # Author: noisywiz
 
+
+class PipeExtension:
+    PIPE_LIST = [
+        "PP_PN20",
+        "VGP_GOST_3262_75"
+    ]
+    NAME = ""
+    ABS_ROUGHNESS = 0.0
+    INNER_DIAMETER = 0.0
+    OUTER_DIAMETER = 0.0
+
+    def get_in_d(self):
+        return self.INNER_DIAMETER
+
+    def get_out_d(self):
+        return self.OUTER_DIAMETER
+
+    def get_abs_roughness(self):
+        return self.ABS_ROUGHNESS
+
+
+class PipeCreator:
+    def factory_method(index):
+        if index == 0:
+            return PP_PN20()
+        else:
+            if index == 1:
+                return VGP_GOST_3262_75()
+            else:
+                return VGP_GOST_3262_75()
+
+
+class PP_PN20(PipeExtension):
+    NAME = "PP_PN20"
+    ABS_ROUGHNESS = 0.00001
+    INNER_DIAMETER = [
+        14.4,  # 20x2.8
+        18.0,  # 25x3.5
+        23.2,  # 32x4.4
+        29.0,  # 40x5.5
+        36.2,  # 50x6.9
+        45.8   # 63x8.6
+    ]
+    OUTER_DIAMETER = [
+        "20x2.8",
+        "25x3.5",
+        "32x4.4",
+        "40x5.5",
+        "50x6.9",
+        "63x8.6"
+    ]
+
+
+class VGP_GOST_3262_75(PipeExtension):
+    NAME = "VGP_GOST_3262_75"
+    ABS_ROUGHNESS = 0.0005
+    INNER_DIAMETER = [
+        15.0,  # 15x2.8
+        20.0,  # 20x2.8
+        25.0,  # 25x3.2
+        32.0,  # 32x3.2
+        40.0,  # 40x3.5
+        50.0,  # 50x3.5
+        65.0,  # 65x4.0
+        80.0,  # 80x4.0
+        100.0, # 100x4.5
+        125.0, # "125x4.5"
+        150.0 # "150x4.5"
+    ]
+    OUTER_DIAMETER = [
+        "15x2.8",
+        "20x2.8",
+        "25x3.2",
+        "32x3.2",
+        "40x3.5",
+        "50x3.5",
+        "65x4.0",
+        "80x4.0",
+        "100x4.5",
+        "125x4.5",
+        "150x4.5"
+    ]
+
+
+# Dimension extension
+class DimensionExtension:
+    MM_TO_M = 0.001
+    M3H_TO_M3S = 0.000277778
+    LS_TO_M3S = 0.001
+    MWC_TO_PA = 9806.805923311 # meter water column to Pa
+    # Standard dimension:
+    X_DMN = [
+        MM_TO_M,  # * mm -> m
+        1,  # nope
+    ]
+    G_DMN = [
+        M3H_TO_M3S,
+        LS_TO_M3S,
+        1,  # nope
+    ]
+
+    def convert(cls, x, x_dmn, atr=1.0):
+        return x*x_dmn*atr
+
+
 class PoringHydraulic:
 
     def __init__(self, k_r_src, nu_src, ro_src, d_src, g_src, ksi_src):
@@ -84,102 +189,3 @@ class PoringHydraulic:
             except ZeroDivisionError:
                 self.lodrop = 0.0
             return self.lodrop
-
-    
-class PipeExtension:
-    PIPE_LIST = [
-        "PP_PN20",
-        "VGP_GOST_3262_75"
-    ]
-    NAME = ""
-    ABS_ROUGHNESS = 0.0
-    INNER_DIAMETER = 0.0
-    OUTER_DIAMETER = 0.0
-
-    def get_in_d(self):
-        return self.INNER_DIAMETER
-
-    def get_out_d(self):
-        return self.OUTER_DIAMETER
-
-    def get_abs_roughness(self):
-        return self.ABS_ROUGHNESS
-
-
-class PipeCreator:
-    def factory_method(index):
-        if index == 0:
-            return PP_PN20()
-        else:
-            if index == 1:
-                return VGP_GOST_3262_75()
-            else:
-                return VGP_GOST_3262_75()
-
-
-class PP_PN20(PipeExtension):
-    NAME = "PP_PN20"
-    ABS_ROUGHNESS = 0.00001
-    INNER_DIAMETER = [
-        14.4,  # 20x2.8
-        18.0,  # 25x3.5
-        23.2,  # 32x4.4
-        29.0,  # 40x5.5
-        36.2,  # 50x6.9
-        45.8   # 63x8.6
-    ]
-    OUTER_DIAMETER = [
-        "20x2.8",
-        "25x3.5",
-        "32x4.4",
-        "40x5.5",
-        "50x6.9",
-        "63x8.6"
-    ]
-
-
-class VGP_GOST_3262_75(PipeExtension):
-    NAME = "VGP_GOST_3262_75"
-    ABS_ROUGHNESS = 0.0005
-    INNER_DIAMETER = [
-        9.4,  # 15x2.8
-        14.4,  # 20x2.8
-        18.6,  # 25x3.2
-        25.6,  # 32x3.2
-        33.0,  # 40x3.5
-        43.0,  # 50x3.5
-        57.0,  # 65x4.0
-        72.0,  # 80x4.0
-        91.0   # 100x4.5
-    ]
-    OUTER_DIAMETER = [
-        "15x2.8",
-        "20x2.8",
-        "25x3.2",
-        "32x3.2",
-        "40x3.5",
-        "50x3.5",
-        "65x4.0",
-        "80x4.0",
-        "100x4.5"
-    ]
-
-
-# Dimension extension
-class DimensionExtension:
-    MM_TO_M = 0.001
-    M3H_TO_M3S = 0.000277778
-    LS_TO_M3S = 0.001
-    # Standard dimension:
-    X_DMN = [
-        MM_TO_M,  # * mm -> m
-        1,  # nope
-    ]
-    G_DMN = [
-        M3H_TO_M3S,
-        LS_TO_M3S,
-        1,  # nope
-    ]
-
-    def convert(cls, x, x_dmn, atr=1.0):
-        return x*x_dmn*atr
